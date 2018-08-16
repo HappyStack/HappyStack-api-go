65c4d78aa8eebb2fb666d652a0fe41c07bd985a5
s4cha <sachadso@gmail.com>
2018-08-16 23:33:05 +0200

    Serve fake items in JSON locally

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-  fmt.Println("Hello World")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		jsonItems, _ := json.Marshal(fakeItems())
+		w.Write(jsonItems)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func fakeItems() []item {
+	var items []item
+	item1 := item{
+		Name:        "Vitamin D",
+		Dosage:      "2000 UI",
+		TakenToday:  true,
+		ServingSize: 2,
+		ServingType: pill}
+	item2 := item{Name: "Magnesium"}
+	item3 := item{Name: "Zinc"}
+	items = append(items, item1, item2, item3)
+	return items
+}
+
+type servingType string
+
+const (
+	scoop servingType = "scoop"
+	pill  servingType = "pill"
+	drop  servingType = "drop"
+)
+
+type item struct {
+	Name        string      `json:"name"`
+	Dosage      string      `json:"dosage"`
+	TakenToday  bool        `json:"takenToday"`
+	ServingSize int         `json:"servingSize"`
+	ServingType servingType `json:"servingType"` // scoop, pill, drop Todo use enum later
+	Timing      time.Time   `json:"timing"`
 }

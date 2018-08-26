881f99ba0ad908daf17be878ee7277ad8b13a564
s4cha <sachadso@gmail.com>
2018-08-26 14:28:02 +0200

    updates main

--- main.go
+++ main.go
@@ -7,22 +7,6 @@ import (
 	"net/http"
 )
 
-const (
-	privateKeyPath = "keys/app.rsa"
-	publicKeyPath  = "keys/app.rsa.pub"
-)
-
-var VerifyKey, SignKey []byte
-
-type Token struct {
-	Token string `json:"token"`
-}
-
-type UserCredentials struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
 func main() {
 	initKeys()
 	router := NewRouter()

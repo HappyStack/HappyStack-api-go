1e59473e7a283ed7ec351032c5f8914c0d472361
s4cha <sachadso@gmail.com>
2018-08-27 17:21:00 +0200

    Improves DB scripts

--- Database scripts/createDB.sql
+++ Database scripts/createDB.sql
@@ -1,5 +1,11 @@
-CREATE TABLE item (
-    item_id serial PRIMARY KEY,
+CREATE TABLE users (
+    user_id serial PRIMARY KEY,
+    email text UNIQUE
+);
+
+CREATE TABLE items (
+    item_id serial PRIMARY KEY, 
+    user_id integer REFERENCES users,
     name varchar (50) NOT NULL,
     dosage varchar (25),
     taken_today boolean DEFAULT false,
@@ -7,3 +13,10 @@ CREATE TABLE item (
     serving_type varchar(5) check (serving_type in ('scoop', 'pill', 'drop')) DEFAULT 'pill',
     timing time
 );
+
+-- Seed Data
+INSERT INTO users (email) VALUES ('sachadso@gmail.com');
+INSERT INTO users (email) VALUES ('nerv.junker@gmail.com');
+
+-- List all user's items
+SELECT item_id, user_id, name, dosage, taken_today, serving_size, serving_type FROM items WHERE user_id = 1;
